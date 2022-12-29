50d2c00ced028a75f61f5863f983543e8a0e7e8a
Javier Giron <JavierG@ad.mobilehousingoftexas.com>
2022-12-29 10:33:26 -0500

    fix issues about rules

---
+++ aspnet-core/src/Americasa.Demo.Domain.Shared/Errors/Activity/ActivityError.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Americasa.Demo.Errors.Activity
+{
+    public class ActivityError
+    {
+        public string Type { get; set; }
+        public string Name { get; set; }
+        public string Message { get; set; }
+        public string Expression { get; set; }
+
+    }
+}
